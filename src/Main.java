406bb4e920034417453440347cc6986e1ddff660
BT_2N2_02 <sample@bths.edu>
2023-12-15 13:55:49 -0500

    step 71

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String [] args) {
         System.out.println("Today is" + Date.day(12, 15) );
-        System.out.println("hello");
+        System.out.println("string changed");
         System.out.println("yesterday was "+ Date.day(12,14));
         System.out.print("change step 64");
         System.out.println("testing");
