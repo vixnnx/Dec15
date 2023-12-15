ce1fc8ede6422a6ff79590f732338098831753b9
BT_2N2_01 <sample@bths.edu>
2023-12-15 13:57:32 -0500

    change line again

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out.println("Today is" + Date.day(12, 15) );
         System.out.println("string changed");
         System.out.println("yesterday was "+ Date.day(12,14));
-        System.out.print("change step 64");
+        System.out.print("changing");
         System.out.println("changed this line!");
     }
 }
