9e675f84699b0348a8cf3e3a88f2ad804de9e6eb
BT_2N2_01 <sample@bths.edu>
2023-12-15 13:49:33 -0500

    print

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String [] args) {
         System.out.println("Today is" + Date.day(12, 15) );
         System.out.println("hello");
+        System.out.println("yesterday was "+ Date.day(12,14));
     }
 }
