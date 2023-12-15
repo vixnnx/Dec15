606ccff0dc995709c0551d1b154479b1268a3bbf
BT_2N2_01 <sample@bths.edu>
2023-12-15 13:35:27 -0500

    added Date

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String [] args) {
-        System.out.println("Today is" + Date.day() );
+        System.out.println("Today is" + Date.day(12, 15) );
     }
 }
